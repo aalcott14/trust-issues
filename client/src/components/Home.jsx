f14007ef2466a953357f08d7babde0c6b9a76e06
Adam Alcott <aalcott14@gmail.com>
2017-08-10 20:01:06 -0500

    set up api requests to politifact with onclick and render top 5 headlines

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => (
-  <div className="Home">
-    <h1>Pick A Politician</h1>
-  </div>
-);
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headlines: [],
+      showLines: false,
+    };
+    this.getHeadlines = this.getHeadlines.bind(this);
+    this.showHeadlines = this.showHeadlines.bind(this);
+  }
+
+
+  componentWillMount() {
+    this.getHeadlines();
+  }
+
+  getHeadlines() {
+    axios.get('http://www.politifact.com/api/statements/truth-o-meter/json/?n=5')
+      .then((data) => {
+        this.setState({
+          headlines: data,
+        });
+      });
+  }
+
+  showHeadlines() {
+    this.setState({
+      showLines: true,
+    });
+  }
+
+  render() {
+    if (this.state.showLines) {
+      return (
+        <div className="Home">
+          <h1>Check top headlines:</h1>
+          <button onClick={this.showHeadlines}>
+            Get headlines
+          </button>
+          <div>
+            {
+              this.state.headlines.data.map(line =>
+                (<span>
+                  <a href={line.statement_url}>
+                    {line.ruling_headline}
+                  </a><br />
+                </span>),
+              )
+            }
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="Home">
+        <h1>Check top headlines:</h1>
+        <button onClick={this.showHeadlines}>
+          Get headlines
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Home;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json()); // get info from html forms;
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 app.listen('3030', () => {
   console.log('Listening on port 3030...');
