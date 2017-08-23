ce8c61ad4780ddd7ab28ed936542dafbef995134
Adam Alcott <aalcott14@gmail.com>
2017-08-22 20:21:18 -0500

    add drag css and implement testing for component re-rendering

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -40820,23 +40820,74 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(6);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _MuiThemeProvider = __webpack_require__(408);
+
+var _MuiThemeProvider2 = _interopRequireDefault(_MuiThemeProvider);
+
+var _RaisedButton = __webpack_require__(332);
+
+var _RaisedButton2 = _interopRequireDefault(_RaisedButton);
+
+__webpack_require__(481);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Drag = function Drag() {
-  return _react2.default.createElement(
-    "div",
-    { className: "Drag" },
-    _react2.default.createElement(
-      "h1",
-      null,
-      "Drag"
-    )
-  );
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Drag = function (_Component) {
+  _inherits(Drag, _Component);
+
+  function Drag(props) {
+    _classCallCheck(this, Drag);
+
+    var _this = _possibleConstructorReturn(this, (Drag.__proto__ || Object.getPrototypeOf(Drag)).call(this, props));
+
+    _this.state = {
+      test: null
+    };
+    _this.test = _this.test.bind(_this);
+    return _this;
+  }
+
+  _createClass(Drag, [{
+    key: 'test',
+    value: function test() {
+      this.setState({
+        test: 'PLEASE STAY'
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        _MuiThemeProvider2.default,
+        null,
+        _react2.default.createElement(
+          'div',
+          { className: 'drag' },
+          _react2.default.createElement(_RaisedButton2.default, {
+            label: 'TEST BUTTON',
+            primary: true,
+            className: 'test-button',
+            onClick: this.test
+          })
+        )
+      );
+    }
+  }]);
+
+  return Drag;
+}(_react.Component);
 
 exports.default = Drag;
 
@@ -41246,6 +41297,51 @@ var keyOf = function keyOf(oneKeyObj) {
 };
 
 module.exports = keyOf;
+
+/***/ }),
+/* 481 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(482);
+if(typeof content === 'string') content = [[module.i, content, '']];
+// Prepare cssTransformation
+var transform;
+
+var options = {}
+options.transform = transform
+// add the styles to the DOM
+var update = __webpack_require__(272)(content, options);
+if(content.locals) module.exports = content.locals;
+// Hot Module Replacement
+if(false) {
+	// When the styles change, update the <style> tags
+	if(!content.locals) {
+		module.hot.accept("!!../../../node_modules/css-loader/index.js!./Drag.css", function() {
+			var newContent = require("!!../../../node_modules/css-loader/index.js!./Drag.css");
+			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+			update(newContent);
+		});
+	}
+	// When the module is disposed, remove the <style> tags
+	module.hot.dispose(function() { update(); });
+}
+
+/***/ }),
+/* 482 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(271)(undefined);
+// imports
+
+
+// module
+exports.push([module.i, ".drag {\n  padding: 10px;\n  text-align: center;\n}\n\n.test-button {\n  margin-top: 50px;\n}\n", ""]);
+
+// exports
+
 
 /***/ })
 /******/ ]);
--- client/src/components/Drag.jsx
+++ client/src/components/Drag.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
-const Drag = () => (
-  <div className="Drag">
-    <h1>Drag</h1>
-  </div>
-);
+import './Drag.css';
+
+class Drag extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: null,
+    };
+    this.test = this.test.bind(this);
+  }
+
+  test() {
+    this.setState({
+      test: 'PLEASE STAY',
+    });
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className="drag">
+          <RaisedButton
+            label="TEST BUTTON"
+            primary
+            className="test-button"
+            onClick={this.test}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
 
 export default Drag;
